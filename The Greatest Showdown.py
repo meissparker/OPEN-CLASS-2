ec7ad427d1f4823ae162306556db43403cfad598
meissparker <meissparker@gmail.com>
2024-08-09 21:26:40 -0500

    second commit

--- The Greatest Showdown.py
+++ The Greatest Showdown.py
@@ -11,7 +11,7 @@ elif number2 >= number1 and number2 >= number3:
 elif number3 >= number1 and number3 >= number2:
     print("The largest number is", number3)
 
-#Task 2 Identify the greatest
+#Task 2 Identify the smallest
 
 if number1 <= number2 and number1 <= number3:
     print("The smallest number is", number1)
